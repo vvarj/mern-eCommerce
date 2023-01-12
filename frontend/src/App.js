900fc17e5aa81e58eff546849c222cffd413f515
arjun <iamarjunvv@gmail.com>
2023-01-12 13:43:03 +0530

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
