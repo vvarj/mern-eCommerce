eb83becf01a9ae5813b6c82da453fcc560a18d54
arjun <iamarjunvv@gmail.com>
2023-01-15 21:53:15 +0530

    add-react-bootstrap-ui

--- README.md
+++ README.md
@@ -24,4 +24,7 @@
 9. Manage State By Reducer Hook
    1. define reducer
    2. update fetch data
-   3. get state from usReducer
+   3. get state from useReducer
+10. Add bootstrap UI Framework
+    1. npm install react-bootstrap bootstrap
+    2. update App.js
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />}></Route>
-            <Route path="/product/:slug" element={<ProductScreen />}></Route>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />}></Route>
+              <Route path="/product/:slug" element={<ProductScreen />}></Route>
+            </Routes>
+          </Container>
         </main>
+        <footer className="text-center">All rights reserved</footer>
       </div>
     </BrowserRouter>
   );
